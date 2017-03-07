900fccae1045026ebba4cf6183d5fda3551eeafe
Sun Tao[孙涛] <st@arcvideo.com>
2017-03-07 20:50:43 +0800

    proxy framework

---
+++ src/main/java/com/arcvideo/smartweb/framework/proxy/Proxy.java
@@ -0,0 +1,10 @@
+package com.arcvideo.smartweb.framework.proxy;
+
+/**
+ * Created by st@arcvideo.com on 2017/3/7.
+ */
+public interface Proxy {
+
+    void doProxy(ProxyChain proxyChain);
+
+}
---
+++ src/main/java/com/arcvideo/smartweb/framework/proxy/ProxyChain.java
@@ -0,0 +1,11 @@
+package com.arcvideo.smartweb.framework.proxy;
+
+/**
+ * Created by st@arcvideo.com on 2017/3/7.
+ */
+public class ProxyChain {
+
+    private Class<?> targetClass;
+    private Object targetObject;
+
+}
